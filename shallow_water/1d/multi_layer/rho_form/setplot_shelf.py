5aa5c66d8f8af091c4a3562c04e46a078b29488b
Kyle Mandli <mandli@amath.washington.edu>
2011-03-13 18:06:26 -0700

    converted and added plotting functionality

--- shallow_water/1d/multi_layer/rho_form/setplot_shelf.py
+++ shallow_water/1d/multi_layer/rho_form/setplot_shelf.py
@@ -19,6 +19,8 @@ from pyclaw.plotters import geoplot, colormaps
 
 # matplotlib.rcParams['figure.figsize'] = [6.0,10.0]
 
+g = 9.81
+
 #--------------------------
 def setplot(plotdata):
 #--------------------------
@@ -30,6 +32,22 @@ def setplot(plotdata):
     
     """
 
+    def add_dashes(current_data):
+        from pylab import ylim,plot
+        plot([-30000,-30000], [-1,1],'k--')
+        
+    def kinetic_energy(current_data):
+        q = current_data.q
+        h = eta_1 - bathy(current_data)
+        u = 0.5 * (u_1(current_data) + u_2(current_data))
+        return np.sum(0.5*h*u**2)
+        
+    def potential_energy(current_data):
+        return np.sum(0.5*eta_1(current_data)**2)
+
+    def total_energy(current_data):
+        return kinetic_energy(current_data) + potential_energy(current_data)
+
     def km_labels(current_data):
         r"""Flips xaxis and labels with km"""
         mpl.xlabel('km')
@@ -37,13 +55,6 @@ def setplot(plotdata):
         labels = np.flipud(locs)/1.e3
         mpl.xticks(locs,labels)
         jump_afteraxes(current_data)
-
-    def jump_afteraxes(current_data):
-        # Plot position of jump on plot
-        mpl.hold(True)
-        mpl.plot([370e3,370e3],[-6,6],'k--')
-        mpl.plot([0.0,400e3],[0.0,0.0],'k--')
-        mpl.hold(False)
 
     def bathy(current_data):
         out_dir = current_data.plotdata.outdir
@@ -74,276 +85,71 @@ def setplot(plotdata):
         return u_2
 
     plotdata.clearfigures()  # clear any old figures,axes,items data
-    
-    # Settings
-    xlimits = [0.0,400e3]
-    ylimits_depth = [-4000.0,100.0]
-    xlimits_zoomed = xlimits
-    ylimits_surface_zoomed = [-0.5,0.5]
-    ylimits_internal_zoomed = [-306,-294]
-    ylimits_velocities = [-1.0,1.0]
-    
-    # ========================================================================
-    #  Fill plot
-    # ========================================================================
-    plotfigure = plotdata.new_plotfigure(name='full',figno=0)
-    
-    plotaxes = plotfigure.new_plotaxes()
-    plotaxes.axescmd = 'subplot(2,1,1)'
-    plotaxes.title = 'Multilayer Surfaces'
-    plotaxes.xlimits = xlimits
-    plotaxes.ylimits = ylimits_depth
-    plotaxes.afteraxes = km_labels
-     
-    # Top layer
-    plotitem = plotaxes.new_plotitem(plot_type='1d_fill_between')
-    plotitem.plot_var = eta_1
-    plotitem.plot_var2 = eta_2
-    plotitem.color = (0.2,0.8,1.0)
-    plotitem.show = True
-    
-    # Bottom Layer
-    plotitem = plotaxes.new_plotitem(plot_type='1d_fill_between')
-    plotitem.plot_var = eta_2
-    plotitem.plot_var2 = bathy
-    plotitem.color = 'b'
-    plotitem.show = True
-    
-    # Plot bathy
-    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    plotitem.plot_var = bathy
-    plotitem.color = 'k'
-    # plotitem.plotstyle = '-'
-    plotitem.show = True
-    
-    # Plot line in between layers
-    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    plotitem.plot_var = eta_2
-    plotitem.color = 'k'
-    # plotitem.plotstyle = 'o'
-    plotitem.show = True
-    
-    # Plot line on top layer
-    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    plotitem.plot_var = eta_1
-    plotitem.color = 'k'
-    # plotitem.plotstyle = 'x'
-    plotitem.show = True
-    
-    # Layer Velocities
-    plotaxes = plotfigure.new_plotaxes()
-    plotaxes.axescmd = 'subplot(2,1,2)'
-    plotaxes.title = "Layer Velocities"
-    plotaxes.xlimits = xlimits
-    plotaxes.afteraxes = km_labels
-    
-    # Bottom layer
-    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    plotitem.plot_var = u_2
-    plotitem.color = 'b'
-    plotitem.show = True
 
-    # Top layer
-    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    plotitem.color = (0.2,0.8,1.0)
-    plotitem.plot_var = u_1
-    plotitem.show = True
+    plotdata.afterframe = "print 'PE = %g, KE = %g, total = %23.16e' % (PE,KE,total)"
     
-    # ========================================================================
-    #  Fill plot zoom
-    # ========================================================================
-    plotfigure = plotdata.new_plotfigure(name='full_zoom',figno=1)
-    
-    plotaxes = plotfigure.new_plotaxes()
-    plotaxes.axescmd = 'subplot(3,1,1)'
-    plotaxes.title = 'Top Surface'
-    plotaxes.xlimits = xlimits_zoomed
-    plotaxes.ylimits = ylimits_surface_zoomed
-    plotaxes.afteraxes = km_labels
-     
-    # Top layer
-    plotitem = plotaxes.new_plotitem(plot_type='1d_fill_between')
-    plotitem.plot_var = eta_1
-    plotitem.plot_var2 = eta_2
-    plotitem.color = (0.2,0.8,1.0)
-    plotitem.show = True
-    
-    # Bottom Layer
-    plotitem = plotaxes.new_plotitem(plot_type='1d_fill_between')
-    plotitem.plot_var = eta_2
-    plotitem.plot_var2 = bathy
-    plotitem.color = 'b'
-    plotitem.show = True
-    
-    # Plot bathy
-    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    plotitem.plot_var = bathy
-    plotitem.color = 'k'
-    # plotitem.plotstyle = '-'
-    plotitem.show = True
-    
-    # Plot line in between layers
-    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    plotitem.plot_var = eta_2
-    plotitem.color = 'k'
-    plotitem.plotstyle = '+'
-    plotitem.show = True
-    
-    # Plot line on top layer
-    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    plotitem.plot_var = eta_1
-    plotitem.color = 'k'
-    plotitem.plotstyle = 'x'
-    plotitem.show = True
-    
-    plotaxes = plotfigure.new_plotaxes()
-    plotaxes.axescmd = 'subplot(3,1,2)'
-    plotaxes.title = 'Internal Surface'
-    plotaxes.xlimits = xlimits_zoomed
-    plotaxes.ylimits = ylimits_internal_zoomed
-    plotaxes.afteraxes = km_labels
-     
-    # Top layer
-    plotitem = plotaxes.new_plotitem(plot_type='1d_fill_between')
-    plotitem.plot_var = eta_1
-    plotitem.plot_var2 = eta_2
-    plotitem.color = (0.2,0.8,1.0)
-    plotitem.show = True
-    
-    # Bottom Layer
-    plotitem = plotaxes.new_plotitem(plot_type='1d_fill_between')
-    plotitem.plot_var = eta_2
-    plotitem.plot_var2 = bathy
-    plotitem.color = 'b'
-    plotitem.show = True
-    
-    # Plot bathy
-    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    plotitem.plot_var = bathy
-    plotitem.color = 'k'
-    # plotitem.plotstyle = '-'
-    plotitem.show = True
-    
-    # Plot line in between layers
-    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    plotitem.plot_var = eta_2
-    plotitem.color = 'k'
-    plotitem.plotstyle = '+'
-    plotitem.show = True
-    
-    # Plot line on top layer
-    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    plotitem.plot_var = eta_1
-    plotitem.color = 'k'
-    plotitem.plotstyle = 'x'
-    plotitem.show = True
-    
-    # Layer Velocities
-    plotaxes = plotfigure.new_plotaxes()
-    plotaxes.axescmd = 'subplot(3,1,3)'
-    plotaxes.title = "Layer Velocities"
-    plotaxes.xlimits = xlimits_zoomed
-    # plotaxes.ylimits = ylimits_velocities
-    plotaxes.afteraxes = jump_afteraxes
-    
-    # Bottom layer
-    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    plotitem.plot_var = u_2
-    plotitem.color = 'b'
-    plotitem.plotstyle = '+-'
-    plotitem.show = True
+    # Figure for eta
+    plotfigure = plotdata.new_plotfigure(name='eta', figno=0)
+    plotfigure.kwargs = {'figsize':(8,3)}
 
-    # Top layer
-    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    plotitem.color = (0.2,0.8,1.0)
-    plotitem.plot_var = u_1
-    plotitem.plotstyle = 'x-'
-    plotitem.show = True
-    
-    # # Wind plot
-    # plotaxes = plotfigure.new_plotaxes()
-    # plotaxes.axescmd = 'subplot(1,2,2)'
-    # plotaxes.title = "Wind Velocity"
-    # plotaxes.xlimits = 'auto'
-    # plotaxes.ylimits = [-5.0,5.0]
-    # 
-    # plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    # plotitem.plot_var = 4
-    # plotitem.color = 'r'
-    # plotitem.show = True
-    
-    # ========================================================================
-    #  h-values
-    # ========================================================================
-    plotfigure = plotdata.new_plotfigure(name='depths',figno=2)
-    
+    # Set up for axes in this figure:
     plotaxes = plotfigure.new_plotaxes()
-    plotaxes.axescmd = 'subplot(2,1,1)'
-    plotaxes.title = 'Depths'
-    plotaxes.xlimits = xlimits
-    plotaxes.afteraxes = jump_afteraxes
-     
-    # Top layer
+    plotaxes.axescmd = 'axes([0.1,0.15,0.80,0.75])'
+    def fixfig(current_data):
+        from pylab import xticks,yticks,xlabel,ylabel,savefig,ylim,title
+        t = current_data.t
+        add_dashes(current_data)
+        xticks([-300000,-200000,-100000, -30000],['300','200','100','30','0'],\
+          fontsize=15)
+        ylim(-0.4,0.6)
+        yticks([-0.4,-0.2,0,0.2,0.4],fontsize=15)
+        #xlabel('kilometres offshore', fontsize=15)
+        ylabel('Metres', fontsize=15)
+        title('Surface at t = %i seconds' % int(t),fontsize=20)
+        # savefig('shelf%s.eps' % str(current_data.frameno).zfill(2))
+
+    plotaxes.afteraxes = fixfig
+    #plotaxes.xlimits = [-150.e3, 50e3]
+    #plotaxes.ylimits = [-0.4, 0.4]
+    plotaxes.title = 'Surface'
+
+    # Set up for item on these axes:
     plotitem = plotaxes.new_plotitem(plot_type='1d')
-    plotitem.plot_var = 0
+    def surface(current_data):
+        eta = B + current_data.q[:,0]
+        return eta
+    plotitem.plot_var = eta_1
     plotitem.plotstyle = '-'
-    plotitem.color = (0.2,0.8,1.0)
-    plotitem.show = True
-    
-    # Bottom layer
-    plotitem = plotaxes.new_plotitem(plot_type='1d')
-    plotitem.plot_var = 2
     plotitem.color = 'b'
-    plotitem.plotstyle = '-'
-    plotitem.show = True
+    plotitem.kwargs = {'linewidth':2}
+    plotitem.show = True       # show on plot?
     
+    # Figure for energy
+    plotfigure = plotdata.new_plotfigure(name='energy', figno=4)
+    plotfigure.show = True
+
+    # Set up for axes in this figure:
     plotaxes = plotfigure.new_plotaxes()
-    plotaxes.axescmd = 'subplot(2,1,2)'
-    plotaxes.title = 'Depths Zoomed'
-    plotaxes.afteraxes = jump_afteraxes
-    # plotaxes.xlimits = [0.0,1.0]
-    # plotaxes.ylimits = [-1.0,0.5]
-    plotaxes.xlimits = xlimits_zoomed
-    # plotaxes.xlimits = [0.0,2000.0]
-    # plotaxes.ylimits = [-2000.0,100.0]
-     
-    # Top layer
+    plotaxes.afteraxes = add_dashes
+    #plotaxes.xlimits = [-150.e3, 50e3]
+    plotaxes.ylimits = 'auto'
+    plotaxes.title = 'Energy'
+
+    # Set up for item on these axes:
     plotitem = plotaxes.new_plotitem(plot_type='1d')
-    plotitem.plot_var = 0
-    plotitem.plotstyle = 'x'
-    plotitem.color = (0.2,0.8,1.0)
-    plotitem.show = True
-    
-    # Bottom layer
-    plotitem = plotaxes.new_plotitem(plot_type='1d')
-    plotitem.plot_var = 2
+    plotitem.plot_var = total_energy
+    plotitem.plotstyle = '-o'
     plotitem.color = 'b'
-    plotitem.plotstyle = '+'
-    plotitem.show = True
+    plotitem.show = True       # show on plot?
     
-    # ========================================================================
-    #  Plot Wind Velocity
-    # ========================================================================
-    # plotfigure = plotdata.new_plotfigure(name='wind',figno=2)
-    # plotfigure.show = True
-    # 
-    # plotaxes = plotfigure.new_plotaxes()
-    # plotaxes.title = "Wind Velocity"
-    # plotaxes.xlimits = 'auto'
-    # plotaxes.ylimits = [-5.0,5.0]
-    # 
-    # plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
-    # plotitem.plot_var = 4
-    # plotitem.color = 'r'
-    # plotitem.show = True
-    
+
     # Parameters used only when creating html and/or latex hardcopy
     # e.g., via pyclaw.plotters.frametools.printframes:
 
     plotdata.printfigs = True                # print figures
     plotdata.print_format = 'png'            # file format
     plotdata.print_framenos = 'all'          # list of frames to print
-    plotdata.print_fignos = 'all'            # list of figures to print
+    plotdata.print_fignos = [2,3]            # list of figures to print
     plotdata.html = True                     # create html files of plots?
     plotdata.html_homelink = '../README.html'   # pointer for top of index
     plotdata.latex = True                    # create latex file of plots?
