8a399006ee4f4969da41e1e5b773ec5d68ad7b1e
Kyle T. Mandli <kyle.mandli@gmail.com>
2011-09-22 15:23:19 -0500

    Changed output frames to all

--- apps/multi_layer/2d/hurricane/setplot.py
+++ apps/multi_layer/2d/hurricane/setplot.py
@@ -1230,8 +1230,8 @@ def setplot(plotdata):
 
     plotdata.printfigs = True                # print figures
     plotdata.print_format = 'png'            # file format
-    #plotdata.print_framenos = 'all'          # list of frames to print
-    plotdata.print_framenos = [48,88,128,168]
+    plotdata.print_framenos = 'all'          # list of frames to print
+    #plotdata.print_framenos = [48,88,128,168]
     plotdata.print_fignos = 'all'            # list of figures to print
     plotdata.html = True                    # create html files of plots?
     plotdata.latex = False                   # create latex file of plots?
